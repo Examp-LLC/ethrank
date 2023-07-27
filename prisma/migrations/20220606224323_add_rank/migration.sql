a61b30b3fd218a702a627c883f4000a91c4d6afe
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2023-07-27 14:35:42 -0700

    feat(prisma): add index for season and address

---
+++ prisma/migrations/20220606224323_add_rank/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Badge` ADD COLUMN `rank` INTEGER NOT NULL DEFAULT 0;
---
+++ prisma/migrations/20230727212315_add_address_season_index/migration.sql
@@ -0,0 +1,2 @@
+-- CreateIndex
+CREATE INDEX `Address_address_season_idx` ON `Address`(`address`, `season`);
