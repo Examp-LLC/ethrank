9c00ae0f3b84bdfbb2d92427efabc9a9dbeae372
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2025-05-20 17:00:27 -0700

    Add Prisma Next.js monorepo workaround plugin to fix Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.m?js$/,
       type: 'javascript/auto',
@@ -14,7 +16,12 @@ module.exports = {
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
-    })
+    });
+    
+    // Add PrismaPlugin for server-side rendering
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
 
     return config;
   },
